83f4d01cd45309d6bbea00c931513684e52a2498
Hiroshi Sawai <info@info-town.jp>
2018-12-29 09:09:14 +0900

    回答実装

--- src/Nagoyaphp13.php
+++ src/Nagoyaphp13.php
@@ -4,24 +4,14 @@ namespace Ttskch\Nagoyaphp13;
 
 class Nagoyaphp13
 {
-    /**
-     * @var Map
-     */
-    private $map;
-
-    public function __construct()
-    {
-        $this->map = new Map();
-    }
-
     public function run(string $input): string
     {
+        $map = new Map();
         $commands = str_split($input);
-
         foreach ($commands as $command) {
-            $this->map->$command();
+            $map->rotate($command);
         }
+        return $map->format();
 
-        return (string)$this->map;
     }
 }
